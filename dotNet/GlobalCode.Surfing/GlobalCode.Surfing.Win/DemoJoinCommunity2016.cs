19c0a581ec7abe1dffef77572688f527338840d3
Marcelo Paiva <marcelo@softprime.com.br>
2016-05-20 10:14:59 -0300

    Including App to test MQTT

---
+++ dotNet/GlobalCode.Surfing/GlobalCode.Surfing.Win/DemoJoinCommunity2016.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GlobalCode.Surfing.Device;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace GlobalCode.Surfing.Win
+{
+    public partial class DemoJoinCommunity2016 : Form
+    {
+        private IoTSurfboard _board;
+        private const string ServerBroker = "iot.eclipse.org";
+        private MqttClient _clientMqtt;
+        private readonly string _clientId = $"Join2016_{Guid.NewGuid()}";
+
+        public DemoJoinCommunity2016()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _board = new IoTSurfboard(txtPorta.Text, 9600);
+                lblID.Text = "Contectado";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnLuz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblLuz.Text = _board.Light().ToString();
+                picLuz.Image = _board.Light() < 100
+                    ? global::GlobalCode.Surfing.Win.Properties.Resources.charge_jarbas_07_02
+                    : global::GlobalCode.Surfing.Win.Properties.Resources.sol;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnTemp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblTemp.Text = _board.Temperature().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRGB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _board.Red(255);
+                Thread.Sleep(500);
+                _board.Red(0);
+                Thread.Sleep(500);
+                _board.Green(255);
+                Thread.Sleep(500);
+                _board.Green(0);
+                Thread.Sleep(500);
+                _board.Blue(255);
+                Thread.Sleep(500);
+                _board.Blue(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _board?.Close();
+                lblID.Text = "Descontectado";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+            // create client instance 
+            _clientMqtt = new MqttClient(ServerBroker);
+
+            // register to message received 
+            _clientMqtt.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+
+            _clientMqtt.Connect(_clientId);
+
+            // subscribe to the topic "/joincommunity/led" with QoS 2 
+            _clientMqtt.Subscribe(new string[] { "/joincommunity/led" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            //ativando timer
+            timerEnvio.Enabled = true;
+            lblMonitor.Text = "Ligado";
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            try
+            {
+                var msg = Encoding.ASCII.GetString(e.Message);
+
+                if (msg == "rgb")
+                {
+                    _board.Red(255);
+                    Thread.Sleep(500);
+                    _board.Red(0);
+                    Thread.Sleep(500);
+                    _board.Green(255);
+                    Thread.Sleep(500);
+                    _board.Green(0);
+                    Thread.Sleep(500);
+                    _board.Blue(255);
+                    Thread.Sleep(500);
+                    _board.Blue(0);
+                }
+                else if (msg == "buzzer")
+                {
+                    _board.Speaker(true);
+                    Thread.Sleep(500);
+                    _board.Speaker(false);
+                }
+                else if (msg == "red")
+                {
+                    _board.Red(255);
+                    Thread.Sleep(500);
+                    _board.Red(0);
+                }
+                else if (msg == "blue")
+                {
+                    _board.Blue(255);
+                    Thread.Sleep(500);
+                    _board.Blue(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFechaMonitor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //desativando timer
+                timerEnvio.Enabled = false;
+
+                _clientMqtt?.Disconnect();
+                lblMonitor.Text = "Desligado";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timerEnvio_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // publish a message on "/joincommunity/luz" topic with QoS 2 
+                _clientMqtt.Publish("/joincommunity/luz", Encoding.UTF8.GetBytes(_board.Light().ToString()),
+                    MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                lblLuz.Text = _board.Light().ToString();
+                picLuz.Image = _board.Light() < 100
+                    ? global::GlobalCode.Surfing.Win.Properties.Resources.charge_jarbas_07_02
+                    : global::GlobalCode.Surfing.Win.Properties.Resources.sol;
+
+                // publish a message on "/joincommunity/temperatura" topic with QoS 2 
+                _clientMqtt.Publish("/joincommunity/temperatura", Encoding.UTF8.GetBytes(_board.Temperature().ToString()),
+                    MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                lblTemp.Text = _board.Temperature().ToString();
+
+                // publish a message on "/joincommunity/potencia" topic with QoS 2 
+                _clientMqtt.Publish("/joincommunity/potencia", Encoding.UTF8.GetBytes(_board.Potentiometer().ToString()),
+                    MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+                lblPotenciometro.Text = _board.Potentiometer().ToString();
+
+                //publish a message on "/joincommunity/alcool" topic with QoS 2
+                _clientMqtt.Publish("/joincommunity/alcool", Encoding.UTF8.GetBytes(_board.Alcohol().ToString()),
+                    MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                lblAlcool.Text = _board.Alcohol().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+    }
+}
---
+++ dotNet/GlobalCode.Surfing/GlobalCode.Surfing.Win/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GlobalCode.Surfing.Device;
+using Microsoft.Azure.Devices.Client;
+using Mono.Security;
+
+namespace GlobalCode.Surfing.Win
+{
+    public partial class Form1 : Form
+    {
+        private const string DeviceConnectionString = "HostName=joincommunity.azure-devices.net;DeviceId=join;SharedAccessKey=FZu4CPCiPbYWw/QD6Ho8P/7nfKmXfykElh2ozbZ98To=";
+        private DeviceClient _deviceClient;
+        private string _retorno;
+
+        IoTSurfboard _board = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConectar_Click(object sender, EventArgs e)
+        {
+            _board = new IoTSurfboard(txtPorta.Text, 9600);
+            lblID.Text = "Contectado";
+            _deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+        }
+
+        private void btnLuz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblLuz.Text = _board.Light().ToString();
+            }
+            catch (Exception ex)
+            {
+                lblLuz.Text = ex.Message;
+            }
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _board?.Close();
+                lblID.Text = "Descontectado";
+                //await _deviceClient.CloseAsync();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btnRGB_Click(object sender, EventArgs e)
+        {
+            _board.Red(255);
+            Thread.Sleep(500);
+            _board.Red(0);
+            Thread.Sleep(500);
+            _board.Green(255);
+            Thread.Sleep(500);
+            _board.Green(0);
+            Thread.Sleep(500);
+            _board.Blue(255);
+            Thread.Sleep(500);
+            _board.Blue(0);
+        }
+
+        private void btnTemp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblTemp.Text = _board.Temperature().ToString();
+            }
+            catch (Exception ex)
+            {
+                lblTemp.Text = ex.Message;
+            }
+        }
+
+
+        private async Task SendEvent(DeviceClient deviceClient, string mensagem)
+        {
+            await deviceClient.OpenAsync();
+            var eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(mensagem));
+            eventMessage.MessageId = Guid.NewGuid().ToString();
+            await deviceClient.SendEventAsync(eventMessage);
+        }
+
+        private async void btnEnviarLuz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await SendEvent(_deviceClient, lblLuz.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void btnReceber_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await ReceiveCommands(_deviceClient, _board);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async Task ReceiveCommands(DeviceClient deviceClient, IoTSurfboard boarIoTSurfboard)
+        {
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync(TimeSpan.FromSeconds(1));
+
+                if (receivedMessage == null) continue;
+
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+
+                if (messageData == "rgb")
+                {
+                    boarIoTSurfboard.Red(255);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Red(0);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Green(255);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Green(0);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Blue(255);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Blue(0);
+                }
+                else if (messageData == "buzzer")
+                {
+                    boarIoTSurfboard.Speaker(true);
+                    Thread.Sleep(500);
+                    boarIoTSurfboard.Speaker(false);
+                }
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+    }
+}
